baebfddadaf500a3f414109ba1c93c42ba857e7a
caotritran <caotritran.14@gmail.com>
2023-03-25 20:25:16 +0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "url" {
   value = "https://${azurerm_cdn_endpoint.tf_endpoint.name}.azureedge.net"
 }
 
-output "cname_url" {
-  value = azurerm_cdn_endpoint_custom_domain.custom_domain.host_name
-}
+# output "cname_url" {
+#   value = azurerm_cdn_endpoint_custom_domain.custom_domain.host_name
+# }
