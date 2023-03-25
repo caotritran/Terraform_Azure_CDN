9ab0f2742ba83b74251c8a597ef60ac5805f9a8e
caotritran <caotritran.14@gmail.com>
2023-03-25 23:14:20 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,8 +23,8 @@ resource "azurerm_cdn_endpoint" "tf_endpoint" {
   origin {
     name      = var.prefix_domain
     host_name = var.domain
-    http_port = ""
-    https_port = ""
+    http_port = 0
+    https_port = 0
   }
 }
 
